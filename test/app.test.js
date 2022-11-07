719f6c1633742af3b8f9a32f6df77d8c5828455b
FabianaAdaro2021 <88856048+FabianaAdaro2021@users.noreply.github.com>
2022-11-07 11:18:24 -0300

    correccion test

--- test/app.test.js
+++ test/app.test.js
@@ -2,14 +2,281 @@ const { expect } = require("chai");
 const { response } = require("express");
 const request = require("supertest");
 const assert = require("chai").assert;
-const { app } = require("../app");
-const { User } = db;
-const bcrypt = require("bcrypt");
+const { app } = require("../app")
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+const MovieController = require("../controllers/movie.controller")
+const RentController = require("../controllers/rent.controller")
+const UserController = require("../controllers/auth.controller")
+const FavouriteController = require("../controllers/fouvoritefilm.controller")
+
+const { server } = require("../app")
+
+const bcrypt = require("bcrypt")
+
+// beforeEach(() => {
+//   prisma.user.delete()
+//   prisma.rents.delete()
+//   prisma.movies.delete()
+//   prisma.favoriteFilms.delete()
+// });
+
+describe("POST /register", () => {
+
+  const userExample = {
+    email: "fabiana@gmail.com",
+    password: "123456",
+    phone: "555-555-555",
+    dni: "12342110",
+  };
+
+  it("should user register", (done) => {
+    request(app)
+      .post("/register")
+      .send(userExample)
+      .expect(201)
+      .then(async (response) => {
+        assert.isTrue(response._body.ok)
+        assert.isNotEmpty(response._body);
+        assert.isNotArray(response._body);
+        assert.containsAllKeys(response._body.usuario, [
+          "email",
+          "password",
+          "phone",
+          "dni",
+          "createdAt",
+          "updatedAt",
+        ])
+        const userDB = await prisma.user.findUnique({
+          where: { email: userExample.email },
+        })
+        assert.exists(userDB);
+        assert.isTrue(
+          bcrypt.compareSync(
+            userExample.password,
+            response._body.usuario.password
+          )
+        );
+      })
+      .then(() => done(), done);
+  })
 
 
-beforeEach(() => {
-  db.sequelize.truncate({ cascade: true });
+  it("Should not allowed user to register twice", (done) => {
+    request(app)
+      .post("/register")
+      .send(userExample)
+      .expect(400)
+      .then((response) => {
+        assert.isString(response._body.errorMessage, "Email, dni or phone is already in use");
+        assert.isNotEmpty(response._body);
+      })
+      .then(() => done(), done);
+  })
+})
+
+
+describe("POST /login", () => {
+  const userExample = {
+    nombre: "fabiana",
+    email: "fabiana@gmail.com",
+    password: "123456",
+    phone: "555-555-555",
+    dni: "12342110",
+  };
+
+  it("should return 200 and a token", (done) => {
+    request(app)
+      .post("/register")
+      .send(userExample)
+      .then((user) => {
+        request(app)
+          .post("/login")
+          .send({ email: userExample.email, password: userExample.password })
+          .expect(200)
+          .then((res) => {
+            assert.isNotEmpty(res._body.token);
+          })
+          .then(() => done(), done);
+      });
+  });
 });
+
+
+describe('POST /favourite/:code', () => {
+
+  const userExample = {
+    nombre: "Fabiana",
+    email: "fabiana@gmail.com",
+    password: "123456",
+    phone: "333-333-333",
+    dni: "12342110",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjp7ImlkIjoyLCJlbWFpbCI6ImNyaXN0aWFuQGdtYWlsLmNvbSIsImRuaSI6IjQzMTIzNDUzIiwicGhvbmUiOiI1NTUtNTU1LTU1NSIsInBhc3N3b3JkIjoiJDJiJDEwJC9rcmxJUUxibTJFMUgvTGkzRDhDUy52T2JxaXB2Mm9mcVhtejg4SE53ZDh6VENxdTgxLjllIiwiY3JlYXRlZEF0IjoiMjAyMi0xMS0wN1QwNTo0NTozNi4xMjlaIiwidXBkYXRlZEF0IjpudWxsLCJyb2xlIjoiVVNFUiJ9LCJpYXQiOjE2Njc3OTk5OTUsImV4cCI6MTczMDAwNzk5NX0.JM-a8pFB1__gynjlXj23MiZu2VsfASMfFdgPOGVO_6w"
+  }
+
+  const movieExample = {
+    codeExample: "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+    codeExample2: "0440483e-ca0e-4120-8c50-4c8cd9b965d6",
+    title: "Castle in the Sky",
+    stock: "5",
+    rentals: "0",
+    review: "Colocar Review"
+  }
+
+  it("Should return 201 and set movie as favourite for logged user with review", done => {
+    request(app)
+      .post(`/favourite/${movieExample.codeExample}`)
+      .send({ review: movieExample.review })
+      .set({ Authorization: `Bearer ${userExample.token}` })
+      .expect(201)
+      .then((response) => {
+        assert.isString(response._body.msg, "Movie Added to Favorites");
+      })
+      .then(() => done(), done);
+  });
+
+  it("Should return 201 and set movie as favourite for logged user without review", done => {
+    request(app)
+      .post(`/favourite/${movieExample.codeExample2}`)
+      .set({ Authorization: `Bearer ${userExample.token}` })
+      .expect(201)
+      .then((response) => {
+        assert.isString(response._body.msg, "Movie Added to Favorites");
+      })
+      .then(() => done(), done);
+  });
+
+
+  it.only("Should not allow to favourite the same movie twice", done => {
+    request(app)
+      .post(`/favourite/${movieExample.codeExample}`)
+      .set({ Authorization: `Bearer ${userExample.token}` })
+      .expect(400)
+      .then((response) => {
+        assert.isString(response._body.errorMessage, "Film is already added to favorite");
+      })
+      .then(() => done(), done);
+  })
+
+});
+
+
+
+// TO-DO
+// Check status
+// Check si se registro el cambio en la DB
+// Check si el registro en la DB es correcto
+
+
+
+
+
+describe('POST /rent/:code', () => {
+
+  // beforeEach(done => {
+  // })
+
+  const userExample = {
+    nombre: "Fabiana",
+    email: "fabiana@gmail.com",
+    password: "123456",
+    phone: "333-333-333",
+    dni: "12342110",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjp7ImlkIjoyLCJlbWFpbCI6ImNyaXN0aWFuQGdtYWlsLmNvbSIsImRuaSI6IjQzMTIzNDUzIiwicGhvbmUiOiI1NTUtNTU1LTU1NSIsInBhc3N3b3JkIjoiJDJiJDEwJDJTYVZZZldYYUphRDNSNzJycHI1bWVnNzZhTU9MZThkUFJSNzJ3bUp4YURpQUl2LlpnMHB1IiwiY3JlYXRlZEF0IjoiMjAyMi0xMS0wN1QwNToxMjozMS4yOTdaIiwidXBkYXRlZEF0IjpudWxsLCJyb2xlIjoiVVNFUiJ9LCJpYXQiOjE2Njc3OTgzNzIsImV4cCI6MTczMDAwNjM3Mn0.f238OC5kynHCvVO_JPl0t-ICSDjG02mV18_XquHIVAA"
+  }
+
+  const movieExample = {
+    codeExample: "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+    title: "Castle in the Sky",
+    stock: "5",
+    rentals: "0",
+    review: "Colocar Review"
+  }
+
+  it("Should return 201 and successfully rent a movie", done => {
+    request(app)
+      .post(`/rent/${movieExample.codeExample}`)
+      .set({ Authorization: `Bearer ${userExample.token}` })
+      .expect(201)
+      .then(async (response) => {
+        const rents = await prisma.rents.findMany()
+        const movies = await prisma.movies.findMany()
+        assert.isString(response._body.msg, "Rented movie")
+        assert.operator(rents[0].id_rent, ">", 0)
+        assert.operator(movies[0].rentals, ">", 0)
+
+      })
+      .then(() => done(), done);
+    //TO_DO
+    //Check status
+    //Chequear si se persistio correctamente la reserva
+    //Chequear que se quito una peli de stock
+    //Chequear que se sumo la renta a las veces alquiladas
+  })
+  it("Should not allow rent if there is no stock", done => {
+    //TO-DO
+  })
+  it("Should not allow rent if movie does not exist", done => {
+    //TO-DO
+  })
+  it("Should not allow non logged user to rent a movie", done => {
+    //TO-DO
+  })
+})
+
+describe("POST /return/:code", done => {
+  beforeEach(done => {
+    // Crear usuario, pelicula, y rentas, una vencida y una sin vencer
+  })
+  it("Should return a rental on time", done => {
+    //TO-DO
+    //Chequear status code 200
+    //Chequear que se devuelva correctamente el precio
+    //Chequear que se restockee correctamente la pelicula
+    //Chequear que se persitio la fecha de devolucion
+  })
+  it("Should return late rental", done => {
+    //TO-DO
+    //Chequear status code 200
+    //Chequear que se devuelva correctamente el precio con el agregado
+    //Chequear que se restockee correctamente la pelicula
+    //Chequear que se persitio la fecha de devolucion
+  })
+  it("Should return a movie that was rented a second time", done => {
+    //TO-DO
+  })
+  it("Should not allow to rent movie twice simultaneously", done => {
+    //TO-DO
+  })
+  it("Should not allow to return already returned movie", done => {
+    //TO-DO
+  })
+  it("Should not allow to return non rented movie", done => {
+    //TO-DO
+  })
+  it("Should not allow non logged user to return a movie", done => {
+    //TO-DO
+  })
+})
+
+describe('GET /favourites', () => {
+  beforeEach(done => {
+    // Crear usuario, pelicula y agregar favoritos
+  })
+  it("Should return 200 status and logged user favourite list", done => {
+    // TO-DO
+    // checkear que sea un array 
+    // checkear que tenga la cantidad correcta de elementos
+    // checkear las clave de cada elemento
+    // checkear que los elementos sean/sea el/los correctos
+  })
+  it("Should forbid access to non logged user", done => {
+    //TO-DO
+    //Chequear status
+    //Chequear mensaje de error
+  })
+})
+
 
 describe("GET /movies", () => {
   it("Should return status 200", (done) => {
@@ -69,183 +336,7 @@ describe("GET /movies/:id", () => {
   });
 });
 
-describe("POST /register", () => {
-  const userExample = {
-    email: "fabiana@gmail.com",
-    password: "1234",
-    phone: "555-555-555",
-    dni: "123456789",
-  };
 
-  it("should return 201", (done) => {
-    request(app)
-      .post("/register")
-      .send(userExample)
-      .expect(201)
-      .then(() => done(), done);
-  });
-
-  it("should user register", (done) => {
-    request(app)
-      .post("/register")
-      .send(userExample)
-      .expect(201)
-        .then(async (response) => {
-          assert.isTrue(response._body.ok)
-        assert.isNotEmpty(response._body); //no esta vacio
-        assert.isNotArray(response._body);
-        assert.containsAllKeys(response._body.usuario, [
-          "email",
-          "password",
-          "phone",
-          "dni",
-          "createdAt",
-          "updatedAt",
-        ]);
-        const userDB = await User.findOne({
-          where: { email: userExample.email },
-        });
-        assert.exists(userDB);
-        assert.isTrue(
-          bcrypt.compareSync(
-            userExample.password,
-            response._body.usuario.password
-          )
-        );
-      })
-      .then(() => done(), done);
-  });
-
-  it("Should not allowed user to register twice", done => {
-    //TO-DO
-    //Check that repeated user doesnt persist
-  })
-});
-
-describe("POST /login", () => {
-  const userExample = {
-    nombre: "Fabiana",
-    email: "fabiana@gmail.com",
-    password: "1234",
-    phone: "555-555-555",
-    dni: "123456789",
-  };
-
-  it("should return 200 and a token", (done) => {
-    request(app)
-      .post("/register")
-      .send(userExample)
-      .then((user) => {
-        request(app)
-          .post("/login")
-          .send({ email: userExample.email, password: userExample.password })
-          .expect(200)
-          .then((res) => {
-            //checks y aserciones
-          })
-          .then(() => done(), done);
-      });
-  });
-});
-
-describe('POST /favourite/:code', () => {
-  beforeEach(done => {
-    //Crear usuario y pelicula
-  })
-  it("Should return 201 and set movie as favourite for logged user with review", done => {
-    // TO-DO
-    // Check status
-    // Check si se registro el cambio en la DB
-    // Check si el registro en la DB es correcto
-  })
-  it("Should return 201 and set movie as favourite for logged user without review", done => {
-    // TO-DO
-    // Check status
-    // Check si se registro el cambio en la DB
-    // Check si el registro en la DB es correcto
-  })
-  it("Should not allow to favourite the same movie twice", done => {
-    //TO-DO, llamar al endpoint con la misma peli 2 veces
-    // Check error status
-    // Check error message
-    // Check db que no se haya persistido un registro
-  })
-})
-
-describe('GET /favourites', () => {
-  beforeEach(done => {
-    // Crear usuario, pelicula y agregar favoritos
-  })
-  it("Should return 200 status and logged user favourite list", done => {
-    // TO-DO
-    // checkear que sea un array 
-    // checkear que tenga la cantidad correcta de elementos
-    // checkear las clave de cada elemento
-    // checkear que los elementos sean/sea el/los correctos
-  })
-  it("Should forbid access to non logged user", done => {
-    //TO-DO
-    //Chequear status
-    //Chequear mensaje de error
-  })
-})
-
-describe('POST /rent/:code', () => {
-  beforeEach(done => {
-    // Crear usuario, pelicula
-    })
-    it("Should return 201 and successfully rent a movie", done => {
-      //TO_DO
-      //Check status
-      //Chequear si se persistio correctamente la reserva
-      //Chequear que se quito una peli de stock
-      //Chequear que se sumo la renta a las veces alquiladas
-    })
-    it("Should not allow rent if there is no stock", done => {
-      //TO-DO
-    })
-    it("Should not allow rent if movie does not exist", done => {
-      //TO-DO
-    })
-    it("Should not allow non logged user to rent a movie", done => {
-      //TO-DO
-    })
-  })
-
-describe("POST /return/:code", done => {
-  beforeEach(done => {
-    // Crear usuario, pelicula, y rentas, una vencida y una sin vencer
-  })
-  it("Should return a rental on time", done => {
-    //TO-DO
-    //Chequear status code 200
-    //Chequear que se devuelva correctamente el precio
-    //Chequear que se restockee correctamente la pelicula
-    //Chequear que se persitio la fecha de devolucion
-  })
-  it("Should return late rental", done => {
-    //TO-DO
-    //Chequear status code 200
-    //Chequear que se devuelva correctamente el precio con el agregado
-    //Chequear que se restockee correctamente la pelicula
-    //Chequear que se persitio la fecha de devolucion
-  })
-  it("Should return a movie that was rented a second time", done => {
-    //TO-DO
-  })
-  it("Should not allow to rent movie twice simultaneously", done => {
-    //TO-DO
-  })
-  it("Should not allow to return already returned movie", done => {
-    //TO-DO
-  })
-  it("Should not allow to return non rented movie", done => {
-    //TO-DO
-  })
-  it("Should not allow non logged user to return a movie", done => {
-    //TO-DO
-  })
-})
 
 describe("Not Found handling", () => {
   it("Should return status 404", (done) => {
@@ -258,3 +349,18 @@ describe("Not Found handling", () => {
       .then(() => done(), done);
   });
 });
+
+Footer
+//© 2022 GitHub, Inc.
+//Footer navigation
+//Terms
+//Privacy
+//Security
+//Status
+//Docs
+//Contact GitHub
+//Pricing
+//API
+//Training
+//Blog
+//About
